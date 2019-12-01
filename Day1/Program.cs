1b583c0d444098368e3dea15802ce12bcbba6a39
Johan Öbrink <johan.erik.obrink@gmail.com>
2019-12-01 14:36:43 +0100

    day1

---
+++ Day1/Program.cs
@@ -0,0 +1,44 @@
+﻿using PuzzleInputParser;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace AdventOfCode2019
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var masses = FileReader.GetValues("./input_part1.txt");
+            var totalFuel = new List<int>();
+
+            foreach(var mass in masses)
+            {
+                var fuel = Convert.ToInt32(Math.Floor(mass / 3d)) - 2;
+                totalFuel.Add(fuel);
+            }
+
+            Console.WriteLine($"total fuel part 1: {totalFuel.Sum()}");
+
+            var fuelsFuel = new List<int>();
+            foreach (var mass in masses)
+            {
+                fuelsFuel.Add(AdditionalFuel(mass));
+            }
+
+            Console.WriteLine($"total fuel part 2: {fuelsFuel.Sum()}");
+        }
+
+        public static int AdditionalFuel(int fuel)
+        {
+            var additionalFuel = Convert.ToInt32(Math.Floor(fuel / 3d)) - 2;
+            if(additionalFuel > 0)
+            {
+                return additionalFuel + AdditionalFuel(additionalFuel);
+            }
+
+            return 0;
+        }
+        
+    }
+}
---
+++ PuzzleInputParser/FileReader.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+
+namespace PuzzleInputParser
+{
+    public static class FileReader
+    {
+        public static List<int> GetValues(string fileName)
+        {
+            var allText = File.ReadAllText(fileName);
+            return allText.Split("\r\n").Select(x => int.Parse(x.Trim())).ToList();
+        }
+    }
+}
