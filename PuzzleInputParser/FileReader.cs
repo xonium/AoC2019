e14859e406acda619b6ad62b1b891121c9f61f0f
Johan Öbrink <johan.erik.obrink@gmail.com>
2019-12-03 08:23:50 +0100

    day3

--- PuzzleInputParser/FileReader.cs
+++ PuzzleInputParser/FileReader.cs
@@ -11,5 +11,18 @@ namespace PuzzleInputParser
             var allText = File.ReadAllText(fileName);
             return allText.Split(separator).Select(x => int.Parse(x.Trim())).ToList();
         }
+
+        public static List<List<string>> GetValues(string fileName, string separator, string listseparator)
+        {
+            var allText = File.ReadAllText(fileName);
+            var returnList = new List<List<string>>();
+            var lists = allText.Split(listseparator);
+            foreach(var list in lists)
+            {
+                returnList.Add(list.Split(separator).Select(x => x.Trim()).ToList());
+            }
+
+            return returnList;
+        }
     }
 }
