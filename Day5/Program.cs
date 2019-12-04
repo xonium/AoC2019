2056cc4eec89a5ce5a84f073ee5a20968ffb9cc8
Johan Öbrink <johan.erik.obrink@gmail.com>
2019-12-04 20:21:57 +0100

    day5

---
+++ Day5/Program.cs
@@ -0,0 +1,13 @@
+﻿using PuzzleInputParser;
+using System;
+
+namespace Day5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var values = FileReader.GetValues("./input.txt", "-");
+        }
+    }
+}
