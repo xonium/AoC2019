0c14bea582bf1a308009c1b3584b5745ad96b311
Johan Öbrink <johan.obrink@teleopti.com>
2019-12-06 12:20:24 +0100

    part1

--- Day6/Program.cs
+++ Day6/Program.cs
@@ -7,49 +7,72 @@ namespace Day6
 {
     class Program
     {
+        public static int Count { get; set; }
         static void Main(string[] args)
         {
             var orbits = FileReader.GetValuesString("./input.txt", "\r\n");
-            var orbitChart = FindOrbits("COM", orbits);
-
+            var orbitChart = FindOrbits("COM", orbits, 0);
+            CountOrbits(orbitChart);
+            Console.WriteLine($"-- TotalOrbits --: {Count}");
 
         }
 
-        private static OrbitChart FindOrbits(string PlanetToFind, List<string> orbits)
+        private static void CountOrbits(OrbitChart orbitChart)
+        {
+            if (orbitChart.InOrbit == null) {
+                Count = Count + orbitChart.Depth;
+                return;
+            }
+            foreach (var inOrbit in orbitChart.InOrbit)
+            {
+                CountOrbits(inOrbit);
+            }
+
+            Count = Count + orbitChart.Depth;
+        }
+
+        private static OrbitChart FindOrbits(string PlanetToFind, List<string> orbits, int depth)
         {
             var planetWithOrbits = orbits.Where(x => x.Contains(PlanetToFind + ")"));
             var orbitChart = new OrbitChart();
+            orbitChart.Depth = depth;
+            Console.WriteLine($"depth: {depth}");
+
             foreach (var orbit in planetWithOrbits)
             {
                 if (orbitChart.PlanetName == null) {
                     orbitChart = new OrbitChart(orbit);
-
-                    var foundOrbits = FindOrbits(orbit.Split(')')[1], orbits);
+                    orbitChart.Depth = depth;
+                    var foundOrbits = FindOrbits(orbit.Split(')')[1], orbits, depth + 1);
                     if(foundOrbits.PlanetName != null)
                     {
                         orbitChart.InOrbit = new List<OrbitChart>
                         {
-                            FindOrbits(orbit.Split(')')[1], orbits)
+                            foundOrbits
                         };
                     }
                     else
                     {
+                        var chart = new OrbitChart(orbit.Split(')')[1]);
+                        chart.Depth = depth + 1;
                         orbitChart.InOrbit = new List<OrbitChart>
                         {
-                            new OrbitChart(orbit.Split(')')[1])
+                            chart
                         };
                     }
                 }
                 else
                 {
-                    var foundOrbits = FindOrbits(orbit.Split(')')[1], orbits);
+                    var foundOrbits = FindOrbits(orbit.Split(')')[1], orbits, depth + 1);
                     if (foundOrbits.PlanetName != null)
                     {
-                        orbitChart.InOrbit.Add(FindOrbits(orbit.Split(')')[1], orbits));
+                        orbitChart.InOrbit.Add(foundOrbits);
                     }
                     else
                     {
-                        orbitChart.InOrbit.Add(new OrbitChart(orbit.Split(')')[1]));
+                        var chart = new OrbitChart(orbit.Split(')')[1]);
+                        chart.Depth = depth + 1;
+                        orbitChart.InOrbit.Add(chart);
                     }
                         
                 }
@@ -75,6 +98,8 @@ namespace Day6
             }
 
             public string PlanetName { get; set; }
+
+            public int Depth { get; set; }
 
             public List<OrbitChart> InOrbit { get; set; }
 
