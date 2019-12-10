3cf85f725a7f14eb42e2fee3bf324e58b6474e18
Johan Öbrink <johan.obrink@teleopti.com>
2019-12-10 08:48:23 +0100

    not working

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -31,21 +31,71 @@ namespace Day10
         private static int ShootAsteroidDetectionRayVersion2((int, int) location, List<(int, int)> asteroidLocations, int maxSizeOfSpace)
         {
             var numbersFound = 1;
-            var currentRayPosition = location;
-
             var angles = new List<(int, int)>
             {
                 (1, 0),
-                (0, 1),
                 (1, 1),
+                (0, 1),
+                (2, 1),
+                (1, 2),
+                (3, 1),
+                (3, 2),
+                (2, 3),
+                (1, 3),
+                (4, 1),
+                (4, 3),
+                (3, 4),
+                (1, 4),
+
+
                 (-1, 0),
+                (-1, 1),
+                (-0, 1),
+                (-2, 1),
+                (-1, 2),
+                (-3, 1),
+                (-3, 2),
+                (-2, 3),
+                (-1, 3),
+                (-4, 1),
+                (-4, 3),
+                (-3, 4),
+                (-1, 4),
+
+                (1, -0),
+                (1, -1),
                 (0, -1),
-                (-1, -1)
+                (2, -1),
+                (1, -2),
+                (3, -1),
+                (3, -2),
+                (2, -3),
+                (1, -3),
+                (4, -1),
+                (4, -3),
+                (3, -4),
+                (1, -4),
+
+                (-1, -0),
+                (-1, -1),
+                (-0, -1),
+                (-2, -1),
+                (-1, -2),
+                (-3, -1),
+                (-3, -2),
+                (-2, -3),
+                (-1, -3),
+                (-4, -1),
+                (-4, -3),
+                (-3, -4),
+                (-1, -4),
             };
 
             foreach(var angle in angles) { 
-            var shotRay = 1;
-                while(shotRay < maxSizeOfSpace)
+                var shotRay = 1;
+                var currentRayPosition = location;
+
+                while (shotRay < maxSizeOfSpace)
                 {
                     currentRayPosition.Item1 += angle.Item1 * shotRay;
                     currentRayPosition.Item2 += angle.Item2 * shotRay;
