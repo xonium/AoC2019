ee86230260a5977f11952f37dc60ebc25c35a3d1
Johan <johan.erik.obrink@gmail.com>
2019-12-10 22:28:43 +0100

    done

--- Day10/Program.cs
+++ Day10/Program.cs
@@ -2,6 +2,7 @@
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Numerics;
 
 namespace Day10
 {
@@ -21,17 +22,89 @@ namespace Day10
 
             //part 2
             var space2 = FileReader.GetValuesList("./input.txt", "\r\n");
-
+            var asteroidLocations2 = GetLocationOfAsteroids(space2);
             space2[bestLocation.Item3][bestLocation.Item2] = "X";
 
             PrintSpace(space2);
-            VaporizeAsteroids(space2, (bestLocation.Item2, bestLocation.Item3));
+            var totalVaporizations = 0;
+            while(asteroidLocations2.Count() > 0)
+            {
+                totalVaporizations = VaporizeAsteroids(space2, (bestLocation.Item2, bestLocation.Item3), asteroidLocations2, totalVaporizations);
+                asteroidLocations2 = GetLocationOfAsteroids(space2);
+            }
         }
 
-        private static void VaporizeAsteroids(List<List<string>> space, (int, int) baseLocation)
+        private static int VaporizeAsteroids(List<List<string>> space, (int, int) baseLocation, List<(int,int)> asteroidLocations, int numbersVaporized = 0)
         {
-            var asteroidLocations = GetLocationOfAsteroids(space);
-            var orderedAngles = CalculateAngles(baseLocation, asteroidLocations).OrderBy(x => x.Item1).ThenBy(x => x.Item2);
+            var angles2 = asteroidLocations
+                .Where(x => x != baseLocation)
+                .Select(x => (x.Item1 - baseLocation.Item1, x.Item2 - baseLocation.Item2))
+                .ToList();
+
+            var angles = CalculateAngles(baseLocation, asteroidLocations);
+            angles.Sort(AnglesSorter);
+
+            foreach(var angle in angles)
+            {
+                var asteriodsInLine = angles2.Where(location => NormalizedVector(location) == angle).ToList();
+                asteriodsInLine.Sort(SortByClosest);
+                var location = asteriodsInLine.First();
+
+                numbersVaporized++;
+                space[location.Item2 + baseLocation.Item2][location.Item1 + baseLocation.Item1] = ".";
+                Console.WriteLine($"Vaporized {numbersVaporized} is at {location.Item1 + baseLocation.Item1},{location.Item2 + baseLocation.Item2}");
+
+                if(numbersVaporized == 200)
+                {
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine($"Final answer: {(location.Item1 + baseLocation.Item1) * 100 + (location.Item2 + baseLocation.Item2)}");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                    Console.WriteLine("******************");
+                }
+            }
+
+            return numbersVaporized;
+        }
+
+        private static int SortByClosest((int, int) asteroidOne, (int, int) asteroidTwo)
+        {
+            var asteroidVectorOne = new Vector2(asteroidOne.Item1, asteroidOne.Item2);
+            var asteroidVectorTwo = new Vector2(asteroidTwo.Item1, asteroidTwo.Item2);
+
+            if (asteroidVectorOne.Length() > asteroidVectorTwo.Length())
+                return 1;
+
+            return -1;
+        }
+
+        private static int AnglesSorter((int, int) angleOne, (int, int) angleTwo)
+        {
+            if (GetAngle(angleOne) > GetAngle(angleTwo)) return 1;
+            
+            return -1;
+        }
+
+        private static double GetAngle((int, int) angle)
+        {
+            var radian1 = Math.Atan2(angle.Item2, angle.Item1);
+            var degreeResult = radian1 * (180.0 / Math.PI);
+            if(degreeResult < 0)
+            {
+                if(Math.Abs(degreeResult) < 90)
+                {
+                    return 90 + degreeResult;
+                }
+                return 360 + degreeResult + 90 == 360 ? 0 : 360 + degreeResult + 90;
+            }
+
+            return degreeResult +90;
         }
 
         private static (int, int, int) GetBestLocation(List<List<string>> space)
@@ -77,17 +150,23 @@ namespace Day10
 
             foreach(var angle in angles)
             {
-                var commonDivisor = gcf(angle.Item1, angle.Item2);
-                returnAngles.Add(
-                    (commonDivisor > 0 ? angle.Item1 / commonDivisor : -(angle.Item1 / commonDivisor),
-                     commonDivisor > 0 ? angle.Item2 / commonDivisor : -(angle.Item2 / commonDivisor))
-                    );
+                var vector = NormalizedVector(angle);
+                returnAngles.Add((vector.Item1,vector.Item2));
             }
 
             return returnAngles.Distinct().ToList();
         }
 
-        private static int gcf(int a, int b)
+        private static (int,int) NormalizedVector((int,int) angle)
+        {
+            var commonDivisor = GreatestCommonFactor(angle.Item1, angle.Item2);
+            var returnAngleX = commonDivisor > 0 ? angle.Item1 / commonDivisor : -(angle.Item1 / commonDivisor);
+            var returnAngleY = commonDivisor > 0 ? angle.Item2 / commonDivisor : -(angle.Item2 / commonDivisor);
+
+            return (returnAngleX, returnAngleY);
+        }
+
+        private static int GreatestCommonFactor(int a, int b)
         {
             while (b != 0)
             {
